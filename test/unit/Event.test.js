6d34cefa576b9ccc449e797b4c483e629488f028
TerrenceAddison <terrenceaddison32@gmail.com>
2022-08-27 09:18:10 +0700

    Update Event.test.js

--- test/unit/Event.test.js
+++ test/unit/Event.test.js
@@ -14,7 +14,7 @@ const {developmentChains, networkConfig} = require("../../helper-hardhat.config"
         mockV3Aggregator = await ethers.getContract("MockV3Aggregator",deployer);
     })
 
-    describe("constructor", function () {
+    describe("constructor", function () {        
         it("initializes Event correctly", async function () {
             const eventState = await event.getEventState();
 
@@ -46,6 +46,25 @@ const {developmentChains, networkConfig} = require("../../helper-hardhat.config"
             assert.equal(eventDetails, "random event details");
             assert.equal(priceFeed, mockV3Aggregator.address);
         })
+
+        // PLEASE THERE ARE ALWAYS DELAYS IN MINING AND SO THE EVENT TIME MIGHT VARY BY A SEC OR 2 FOR THE TEST
+        // SO JUST CHANGE THE ADD A SECOND IF IT THROWS OR TWO
+        // If ANYONE HAS SUGGESTION HOW TO DO THE DATE CHECKS PLEASE TELL
+        
+        //   it("Should return Event date", async () => {
+        //     const response = await event.getEventDate()
+        //     assert.equal(response.toNumber(), eventDate + 1) //i think there was a delay by miner that added a second
+        // })
+
+        // it("Should return Event start date", async () => {
+        //     const response = await event.getPurchaseStartDate()
+        //     assert.equal(response.toNumber(), purchaseStartDate + 1)
+        // })
+
+        // it("Should return Event end date", async () => {
+        //     const response = await event.getPurchaseEndDate()
+        //     assert.equal(response.toNumber(), purchaseEndDate + 1)
+        // })
     })
 
     describe("update Functions", function () {
@@ -78,14 +97,86 @@ const {developmentChains, networkConfig} = require("../../helper-hardhat.config"
         })
     })
 
-    // describe("purchaseTicket", function () {
-    //     it("reverts when not enough sent", async function () {
-    //     })
-    //     it("reverts when purchase date has passed", async function () {
-            
-    //     })
-    //     it("reverts when purchase date has not started", async function () {
-            
-    //     })
-    // })
+    describe("purchaseTicket", function () {
+        const AMOUNT = ethers.utils.parseEther("100")
+        const today = new Date()
+        var tomorrow = new Date(today)
+        var inFourDays = new Date(today)
+        var nextWeek = new Date(today)
+
+        tomorrow.setDate(today.getDate() + 1)
+        inFourDays.setDate(today.getDate() + 4)
+        nextWeek.setDate(today.getDate() + 7)
+
+        const purchaseStartDate = Math.floor(tomorrow.getTime() / 1000)
+        const purchaseEndDate = Math.floor(inFourDays.getTime() / 1000)
+
+        it("reverts when not enough sent", async function () {
+            await expect(event.purchaseTicket()).to.be.revertedWith(
+                "Event__NotEnoughCryptoSent"
+            )
+        })
+        it("reverts when purchase date has passed", async function () {
+            await network.provider.send("evm_increaseTime", [
+                purchaseEndDate,
+            ])
+            await expect(
+                event.purchaseTicket({ value: AMOUNT })
+            ).to.be.revertedWith("Event__PurchaseDatePassed")
+        })
+        it("reverts when purchase date has not started", async function () {
+            await expect(
+                event.purchaseTicket({ value: AMOUNT })
+            ).to.be.revertedWith("Event__PurchaseDateNotStarted")
+        })
+        it("should purchase ticket", async function () {
+              await network.provider.send("evm_increaseTime", [
+                  purchaseEndDate - purchaseStartDate,
+              ])
+              await event.purchaseTicket({ value: AMOUNT })
+              const ticketPurchased = await event.getTicketPurchased()
+              assert(ticketPurchased, 1)
+              await expect(event.purchaseTicket({ value: AMOUNT })).to.emit(
+                  event,
+                  "TicketPurchase"
+              )
+        })
+        
+    })  
+
+    describe("Withdraw", function () {
+          //WITHDRAW SHOULD THROW, BECUASE OF SIGHT DIFFERENCE OF GAS FEES
+        //   it("Should withdraw", async () => {
+        //     const { deployer } = await getNamedAccounts()
+        //     const ownerBalance = ethers.utils.formatEther(
+        //         await ethers.provider.getBalance(deployer)
+        //     )
+        //     const serviceFeeBalance = ethers.utils.formatEther(
+        //         await ethers.provider.getBalance(await event.getservice())
+        //     )
+
+        //     const getContractBalance = ethers.utils.formatEther(
+        //         await ethers.provider.getBalance(event.address)
+        //     )
+
+        //     await event.withdraw()
+
+        //     const ownerBalanceAfter = ethers.utils.formatEther(
+        //         await ethers.provider.getBalance(deployer)
+        //     )
+
+        //     const serviceFeeBalanceAfter = ethers.utils.formatEther(
+        //         await ethers.provider.getBalance(await event.getservice())
+        //     )
+        //     // Due to gas fees paid ,  i can't really figure out how to test this perfectly
+        //     assert.equal(
+        //         serviceFeeBalanceAfter,
+        //         serviceFeeBalance + getContractBalance / 100
+        //     )
+        //     assert.equal(
+        //         ownerBalanceAfter,
+        //         ownerBalance + (getContractBalance - getContractBalance / 100)
+        //     )
+        // })        
+    })
 });
